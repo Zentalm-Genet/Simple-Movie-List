cf442f7dc60c19d7690404fcf28278b482f907e0
Zentalm Genet <ayuzentalm@gmail.com>
2024-02-14 11:13:44 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Simple Movie List
+
+- In This Lab We will build an interactive web page, focusing on event listeners and event handlers....
+- We’ll practice more vanilla JS DOM manipulation by creating a simple Movie List project.
+- By Using HTML, CSS and JavaScript to make the interface interactive.
+- Users should be able to type in a movie title and click ‘add’ to add a movie onto the list.
+- Clicking on the movie’s title should cross it off. Clicking the ‘x’ button next to the title should remove the movie from the list.
+- We’ll also be making a dynamic notification.
+
+  - file structure
+    - sample image
+    - index.css
+    - index.html
+    - index.js
---
+++ index.js
@@ -0,0 +1,63 @@
+// Step 1: Creating index.js and Connecting it to HTML
+
+console.log("hello world")
+
+const message = document.querySelector('#message');
+
+// Step 2: Creating the addMovie Function
+
+function addMovie(event){
+
+event.preventDefault() // Prevent default form submission behavior
+
+const inputField = document.querySelector('input');
+
+
+if (inputField.value === '') {
+    alert('Please enter a movie name!');
+    return; // Exit function if input is empty
+}
+
+const movie = document.createElement('li')
+const movieTitle = document.createElement('span')
+
+movieTitle.textContent = inputField.value;
+
+movieTitle.addEventListener('click', crossOffMovie)
+
+movie.appendChild(movieTitle);
+
+const deleteBtn = document.createElement('button');
+
+deleteBtn.textContent = 'X';
+
+deleteBtn.addEventListener('click', deleteMovie)
+
+movie.appendChild(deleteBtn);
+
+document.querySelector('ul').appendChild(movie);
+
+inputField.value = '';
+
+}
+
+document.querySelector('form').addEventListener("submit", addMovie)
+
+// step 3: Creating the deleteMovie Function
+
+function deleteMovie (event){
+    event.target.parentNode.remove()
+    message.textContent = 'Movie deleted!';
+}
+
+// Step 4: Adding the Functionality to Mark Movies as Watched
+
+function crossOffMovie(event){
+    event.target.classList.toggle('checked')
+
+    if (event.target.classList.contains('checked')) {
+        message.textContent = 'Movie watched!';
+    } else {
+        message.textContent = 'Movie added back!';
+    }
+}
